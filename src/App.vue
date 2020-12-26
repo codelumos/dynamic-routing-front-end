695219299691c3876f5d9b521bbbb4e78671b7c6
HaoNShi <haonshi@qq.com>
2020-12-26 23:20:23 +0800

    一些前期准备工作

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="拓补图" src="./assets/Topography.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
