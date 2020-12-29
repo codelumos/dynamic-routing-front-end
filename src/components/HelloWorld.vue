2f1df9039e17dd1cd977b0e9db4d5cf9170d0feb
HaoNShi <haonshi@qq.com>
2020-12-29 16:09:09 +0800

    表单适配Vuetify框架

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
             :src="require('../assets/Topography.png')"
             class="my-3"
             contain
-            height="200"
+            height="400"
         />
       </v-col>
 
@@ -16,25 +16,39 @@
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-              href="https://community.vuetifyjs.com"
-              target="_blank"
-          >Discord Community</a>
+          For help and collaboration with other Vuetify developers
         </p>
       </v-col>
 
-      <v-col
-          class="mb-5"
-          cols="12"
-      >
-        <label for="input_ip_r0">IP地址</label><input id="input_ip_r0" type="text" v-model="ip_r0">
-        <label for="input_pwd_telnet_r0">Telnet密码</label><input id="input_pwd_telnet_r0" type="text"
-                                                                v-model="pwd_telnet_r0">
-        <label for="input_pwd_enable_r0">特权密码</label><input id="input_pwd_enable_r0" type="text"
-                                                            v-model="pwd_enable_r0">
-        <v-btn v-on:click="test">TEST</v-btn>
+      <v-col class="mb-5" cols="12">
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="ip_r0"
+                  label="IP地址"
+                  required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="pwd_telnet_r0"
+                  label="Telnet密码"
+                  required
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                  v-model="pwd_enable_r0"
+                  label="特权密码"
+                  required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-btn color="primary" v-on:click="test">TEST</v-btn>
+        </v-container>
       </v-col>
     </v-row>
   </v-container>
